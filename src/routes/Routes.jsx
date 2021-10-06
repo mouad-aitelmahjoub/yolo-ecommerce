0023eeb32d021984e551e85204a9494d215ad881
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-10-06 11:34:11 +0100

    Layout Setup

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,18 @@
-import React from "react"
+import { Route, Switch } from "react-router-dom"
+import Home from "../pages/Home"
+import Catalogue from "../pages/Catalogue"
+import Cart from "../pages/Cart"
+import Product from "../pages/Product"
 
 const Routes = () => {
-  return <div>Routes</div>
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/catalogue/:slug" exact component={Catalogue} />
+      <Route path="/cart" exact component={Cart} />
+      <Route path="/product" exact component={Product} />
+    </Switch>
+  )
 }
 
 export default Routes
